84dc793221ccadfa32be8352ee42b71955732694
Joe-Story <51319678+Joe-Story@users.noreply.github.com>
2019-06-03 11:00:13 +0100

    Add files via upload

---
+++ ECG_PPG_Comparison/ECG_PPG_Comparison.ino
@@ -0,0 +1,77 @@
+#include "MAX30100.h"
+MAX30100* pulseOxymeter;
+float dcFiltIR;
+
+int sensorPin = A3;
+int ledPin = 13;
+int sensorValue = 0;
+int ecgarray[500];
+int ppgarray[500];
+int i = 0;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Begin Test!");
+  delay(500);
+
+  pulseOxymeter = new MAX30100();
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+
+  for(i = 0; i<500; i = i+1) {
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+    ecgarray[i] = sensorValue;
+    
+    pulseoxymeter_t result = pulseOxymeter->update();
+    dcFiltIR = -(result.dcFilteredIR);
+    ppgarray[i] = dcFiltIR;
+    Serial.println(dcFiltIR);
+    Serial.println(millis());
+    
+    delay(4);
+  }
+
+//  for(i = 0; i<250; i=i+1) {
+//    //Serial.println("Break!");
+//    Serial.print("ECG Value ");
+//    Serial.print(i+1);
+//    Serial.print(": ");
+//    Serial.println(ecgarray[i]);
+//    delay(2);
+//  }
+
+//  for(i = 0; i<250; i=i+1) {
+//    //Serial.println("Break!");
+//    Serial.print("PPG Value ");
+//    Serial.print(i+1);
+//    Serial.print(": ");
+//    Serial.println(ppgarray[i]);
+//    delay(2);
+//  }
+   
+  delay(5000000);
+}
+
+
+//Serial.println(sensorValue);
+
+
+
+
+//  digitalWrite(ledPin, HIGH);
+//  delay(sensorValue/10);
+//  digitalWrite(ledPin, LOW);
+//  delay(sensorValue/10);
+
+
+
+//  if(sensorValue<810) {
+//    Serial.println("Beat!");
+//    Serial.println(millis());
+//    Serial.println("");
+//    delay(150);
+//  }

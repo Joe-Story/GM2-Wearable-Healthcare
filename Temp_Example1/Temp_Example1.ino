d03dd7b23ea509a05e6d75ed3ab594d0a1319347
Joe-Story <51319678+Joe-Story@users.noreply.github.com>
2019-06-03 11:25:18 +0100

    Add files via upload

---
+++ Temp_Example1/Temp_Example1.ino
@@ -0,0 +1,34 @@
+#define VERSION "1.1"
+ 
+#include <inttypes.h>
+#include <Wire.h>
+ 
+#include <lm75.h>
+ 
+TempI2C_LM75 termo = TempI2C_LM75(0x48,TempI2C_LM75::nine_bits);
+ 
+ 
+void setup()
+{
+Serial.begin(9600);
+Serial.println("Start");
+Serial.print("Actual temp ");
+Serial.print(termo.getTemp());
+Serial.println(" oC");
+delay(2000);
+}
+ 
+void loop()
+{
+Serial.print(termo.getTemp());
+Serial.println(" oC");
+delay(200);
+
+if(termo.getTemp()>32){
+  digitalWrite(LED_BUILTIN, HIGH);
+  }
+else {
+  digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+}
